f6bda08218a876942db706626c178cf1a245f882
s25g5d4 <s25g5d4@gmail.com>
2017-06-09 00:38:42 +0800

    3 line version

--- README.md
+++ README.md
@@ -7,10 +7,20 @@ Write just for fun.
 
 ![Running in bash](bash.png)
 
+## 3 Line Version (12-hour Clock with Full 4-digit Year)
+
+![Running in Windows CMD](cmd-3line.png)
+
+![Running in bash](bash-line.png)
+
 ## Run
 
 Node.js >= 6.
 
 `node ascii-clock.js`
+
+3-line:
+
+`node ascii-3line.js`
 
 Note in text mode some characters might not be displayed correctly.
---
+++ ascii-clock-3line.js
@@ -0,0 +1,212 @@
+if (!String.prototype.padEnd) {
+  Object.defineProperty(String.prototype, 'padEnd', {
+    configurable: true,
+    enumerable: false,
+    value: function padEnd(targetLength, padString) {
+      targetLength = targetLength | 0;
+      if (targetLength <= this.length) return String(this);
+      padString = String(padString || ' ');
+      var repeat = Math.ceil((targetLength - this.length) / padString.length);
+      while (repeat--) {
+        padString += padString;
+      }
+      return String(this) + padString.substr(0, targetLength - this.length);
+    },
+    writable: true
+  });
+};
+
+if (!String.prototype.padStart) {
+  Object.defineProperty(String.prototype, 'padStart', {
+    configurable: true,
+    enumerable: false,
+    value: function padStart(targetLength, padString) {
+      targetLength = targetLength | 0;
+      if (targetLength <= this.length) return String(this);
+      padString = String(padString || ' ');
+      var repeat = Math.ceil((targetLength - this.length) / padString.length);
+      while (repeat--) {
+        padString += padString;
+      }
+      return padString.substr(0, targetLength - this.length) + String(this);
+    },
+    writable: true
+  });
+};
+
+const number = [`
+  ██████╗
+ ██╔═████╗
+ ██║██╔██║
+ ████╔╝██║
+ ╚██████╔╝
+  ╚═════╝
+`, `
+     ██╗
+    ███║
+    ╚██║
+     ██║
+     ██║
+     ╚═╝
+`, `
+ ██████╗
+ ╚════██╗
+  █████╔╝
+ ██╔═══╝
+ ███████╗
+ ╚══════╝
+`, `
+ ██████╗
+ ╚════██╗
+  █████╔╝
+  ╚═══██╗
+ ██████╔╝
+ ╚═════╝
+`, `
+ ██╗  ██╗
+ ██║  ██║
+ ███████║
+ ╚════██║
+      ██║
+      ╚═╝
+`, `
+ ███████╗
+ ██╔════╝
+ ███████╗
+ ╚════██║
+ ███████║
+ ╚══════╝
+`, `
+  ██████╗
+ ██╔════╝
+ ███████╗
+ ██╔═══██╗
+ ╚██████╔╝
+  ╚═════╝
+`, `
+ ███████╗
+ ╚════██║
+     ██╔╝
+    ██╔╝
+    ██║
+    ╚═╝
+`, `
+  █████╗
+ ██╔══██╗
+ ╚█████╔╝
+ ██╔══██╗
+ ╚█████╔╝
+  ╚════╝
+`, `
+  █████╗
+ ██╔══██╗
+ ╚██████║
+  ╚═══██║
+  █████╔╝
+  ╚════╝
+`].map(e => e.replace(/^\r?\n|\r?\n$/g, '').split('\n').map(e => e.padEnd(10)));
+
+const colon = `
+
+ ██╗
+ ╚═╝
+ ██╗
+ ╚═╝
+
+`.replace(/^\r?\n|\r?\n$/g, '').split('\n').map(e => e.padEnd(5));
+
+const slash = `
+     ██╗
+    ██╔╝
+   ██╔╝
+  ██╔╝
+ ██╔╝
+ ╚═╝
+`.replace(/^\r?\n|\r?\n$/g, '').split('\n').map(e => e.padEnd(8));
+
+const charA = `
+ █████╗
+██╔══██╗
+███████║
+██╔══██║
+██║  ██║
+╚═╝  ╚═╝
+`.replace(/^\r?\n|\r?\n$/g, '').split('\n').map(e => e.padEnd(8));
+
+const charP = `
+██████╗
+██╔══██╗
+██████╔╝
+██╔═══╝
+██║
+╚═╝
+`.replace(/^\r?\n|\r?\n$/g, '').split('\n').map(e => e.padEnd(8));
+
+const charM = `
+███╗   ███╗
+████╗ ████║
+██╔████╔██║
+██║╚██╔╝██║
+██║ ╚═╝ ██║
+╚═╝     ╚═╝
+`.replace(/^\r?\n|\r?\n$/g, '').split('\n').map(e => e.padEnd(8));
+
+const space = Array(6).fill('   ');
+
+const addAsciiNumber = (ascii, numbers) => {
+  ascii = ascii.map(e => e.slice());
+  numbers.reduce((p, c) => {
+    p.forEach((e, i) => e.push(number[c][i]));
+    return p;
+  }, ascii);
+  return ascii;
+};
+
+const addAsciiChar = (ascii, char) => {
+  ascii = ascii.map(e => e.slice());
+  ascii.forEach((e, i) => {
+    e.push(char[i]);
+  });
+  return ascii;
+};
+
+const displayTime = () => {
+  const date = new Date();
+
+  console.log('\033c\x1B[2J\x1B[0f\u001b[0;0H\n');
+  let asciiDate = Array(6).fill().map(() => []);
+  asciiDate = addAsciiChar(asciiDate, space);
+  asciiDate = addAsciiChar(asciiDate, space);
+  asciiDate = addAsciiChar(asciiDate, space);
+  asciiDate = addAsciiChar(asciiDate, space);
+  asciiDate = addAsciiNumber(asciiDate, (date.getMonth() + 1).toString().padStart(2, '0').split('').map(e => +e));
+  asciiDate = addAsciiChar(asciiDate, slash);
+  asciiDate = addAsciiNumber(asciiDate, (date.getDate()).toString().padStart(2, '0').split('').map(e => +e));
+  asciiDate = addAsciiChar(asciiDate, slash);
+  console.log(asciiDate.map(e => e.join('')).join('\n'));
+  console.log('');
+
+  let asciiYearDayNight = Array(6).fill().map(() => []);
+  asciiYearDayNight = addAsciiChar(asciiYearDayNight, space);
+  asciiYearDayNight = addAsciiChar(asciiYearDayNight, space);
+  asciiYearDayNight = addAsciiNumber(asciiYearDayNight, (date.getFullYear()).toString().split('').map(e => +e));
+  asciiYearDayNight = addAsciiChar(asciiYearDayNight, space);
+  asciiYearDayNight = addAsciiChar(asciiYearDayNight, space);
+  asciiYearDayNight = addAsciiChar(asciiYearDayNight, date.getHours() >= 12 ? charP : charA);
+  asciiYearDayNight = addAsciiChar(asciiYearDayNight, charM);
+  console.log(asciiYearDayNight.map(e => e.join('')).join('\n'));
+  console.log('');
+
+  let asciiTime = Array(6).fill().map(() => []);
+  asciiTime = addAsciiChar(asciiTime, space);
+  asciiTime = addAsciiNumber(asciiTime, ((date.getHours() % 12) || 12).toString().padStart(2, '0').split('').map(e => +e));
+  asciiTime = addAsciiChar(asciiTime, colon);
+  asciiTime = addAsciiNumber(asciiTime, (date.getMinutes()).toString().padStart(2, '0').split('').map(e => +e));
+  asciiTime = addAsciiChar(asciiTime, colon);
+  asciiTime = addAsciiNumber(asciiTime, (date.getSeconds()).toString().padStart(2, '0').split('').map(e => +e));
+  console.log(asciiTime.map(e => e.join('')).join('\n'));
+
+  setTimeout(displayTime, 1000 - (date.getTime() % 1000));
+};
+
+displayTime();
